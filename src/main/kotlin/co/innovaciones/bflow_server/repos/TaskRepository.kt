05d5228d8e5fde94387e0520741570eaeaf20ef1
Sergio Torres <sdtorresl@unal.edu.co>
2024-01-16 16:52:11 -0500

    Fis file deletion

--- src/main/kotlin/co/innovaciones/bflow_server/repos/TaskRepository.kt
+++ src/main/kotlin/co/innovaciones/bflow_server/repos/TaskRepository.kt
@@ -1,5 +1,6 @@
 package co.innovaciones.bflow_server.repos
 
+import co.innovaciones.bflow_server.domain.File
 import co.innovaciones.bflow_server.domain.Job
 import co.innovaciones.bflow_server.domain.Task
 import org.springframework.data.domain.Sort
@@ -7,5 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 
 interface TaskRepository : JpaRepository<Task, Long> {
+
+    fun findAllByAttachments(taskFile: File) : List<Task>
     fun findAllByJob(job: Job, sort: Sort?): List<Task>
 }
