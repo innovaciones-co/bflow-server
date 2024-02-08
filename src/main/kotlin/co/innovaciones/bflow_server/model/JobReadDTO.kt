c995ad13d9a81b351f8dd6ab5443d66300b81fba
Sergio Torres <sdtorresl@unal.edu.co>
2024-02-07 20:07:15 -0500

    Modify job DTOs; added supervisor

---
+++ src/main/kotlin/co/innovaciones/bflow_server/model/JobReadDTO.kt
@@ -0,0 +1,48 @@
+package co.innovaciones.bflow_server.model
+
+import co.innovaciones.bflow_server.model.validators.UniqueJobNumber
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+import java.time.LocalDate
+
+
+class JobDTO {
+
+    var id: Long? = null
+
+    @UniqueJobNumber
+    @NotNull
+    @Size(max = 255)
+    var jobNumber: String? = null
+
+    @Size(max = 255)
+    var name: String? = null
+
+    var plannedStartDate: LocalDate? = null
+
+    var plannedEndDate: LocalDate? = null
+
+    @NotNull
+    @Size(max = 255)
+    var address: String? = null
+
+    var description: String? = null
+
+    @NotNull
+    var buildingType: BuildingType? = null
+
+    @NotNull
+    var client: Long? = null
+
+    @NotNull
+    var user: UserDTO? = null
+
+    var notes: Set<NoteDTO>? = null
+
+    var files: Set<FileDTO>? = null
+
+    var stage: TaskStage = TaskStage.SLAB_DOWN
+
+    var progress: Double = 0.0
+
+}
