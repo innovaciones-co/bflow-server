cc2a0eb829fa73094da4b608bc524faf9ac3de75
Sergio Torres <sdtorresl@unal.edu.co>
2024-04-04 23:05:20 -0500

    Add materials to jobs

--- src/main/kotlin/co/innovaciones/bflow_server/domain/Job.kt
+++ src/main/kotlin/co/innovaciones/bflow_server/domain/Job.kt
@@ -1,12 +1,25 @@
 package co.innovaciones.bflow_server.domain
 
 import co.innovaciones.bflow_server.model.BuildingType
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
+import jakarta.persistence.SequenceGenerator
+import jakarta.persistence.Table
+import java.time.LocalDate
+import java.time.OffsetDateTime
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import java.time.LocalDate
-import java.time.OffsetDateTime
 
 
 @Entity
@@ -88,6 +101,9 @@ class Job {
 
     @OneToMany(mappedBy = "job")
     var tasks: MutableSet<Task>? = null
+
+    @OneToMany(mappedBy = "job")
+    var materials: MutableSet<Item>? = null
 
     @CreatedDate
     @Column(
