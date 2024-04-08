3164f472d64600e2302bf0876a62426e77e62299
Sergio Torres <sdtorresl@unal.edu.co>
2024-04-08 11:57:42 -0500

    Change category relations Add unique validations

--- src/main/kotlin/co/innovaciones/bflow_server/repos/CategoryRepository.kt
+++ src/main/kotlin/co/innovaciones/bflow_server/repos/CategoryRepository.kt
@@ -1,7 +1,18 @@
 package co.innovaciones.bflow_server.repos
 
 import co.innovaciones.bflow_server.domain.Category
+import co.innovaciones.bflow_server.domain.Contact
 import org.springframework.data.jpa.repository.JpaRepository
 
 
-interface CategoryRepository : JpaRepository<Category, Long>
+interface CategoryRepository : JpaRepository<Category, Long> {
+
+    fun findFirstByContact(contact: Contact): Category?
+
+    fun findFirstByParentCategoryAndIdNot(category: Category, id: Long?): Category?
+
+    fun existsByContactId(id: Long?): Boolean
+
+    fun existsByParentCategoryId(id: Long?): Boolean
+
+}
