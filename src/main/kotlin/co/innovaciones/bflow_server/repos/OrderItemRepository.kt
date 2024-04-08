b4ede7c5078067b33fbf14714039732b081fe2e3
Sergio Torres <sdtorresl@unal.edu.co>
2024-04-08 11:58:51 -0500

    Modify items Add validations

--- src/main/kotlin/co/innovaciones/bflow_server/repos/OrderItemRepository.kt
+++
@@ -1,7 +0,0 @@
-package co.innovaciones.bflow_server.repos
-
-import co.innovaciones.bflow_server.domain.OrderItem
-import org.springframework.data.jpa.repository.JpaRepository
-
-
-interface OrderItemRepository : JpaRepository<OrderItem, Long>
