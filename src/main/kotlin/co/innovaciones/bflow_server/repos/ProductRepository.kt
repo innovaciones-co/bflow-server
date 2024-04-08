a6e7016629361d6a84a1c83f38346a6580290398
Sergio Torres <sdtorresl@unal.edu.co>
2024-04-08 12:01:32 -0500

    Add product validations

--- src/main/kotlin/co/innovaciones/bflow_server/repos/ProductRepository.kt
+++ src/main/kotlin/co/innovaciones/bflow_server/repos/ProductRepository.kt
@@ -1,10 +1,13 @@
 package co.innovaciones.bflow_server.repos
 
+import co.innovaciones.bflow_server.domain.Category
 import co.innovaciones.bflow_server.domain.Product
 import org.springframework.data.jpa.repository.JpaRepository
 
 
 interface ProductRepository : JpaRepository<Product, Long> {
+
+    fun findFirstByCategory(category: Category): Product?
 
     fun existsBySkuIgnoreCase(sku: String?): Boolean
 
